cdaf49992ae8600af5e87881ad8172f0388344c8
UnmappedStack <jake.steinburger0@gmail.com>
2025-01-29 11:33:21 +1100

    read from file instead of hardcoded string

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <lexer.h>
 #include <expression.h>
 #include <statement.h>
 
 int main() {
-    char *test = "functionName($5 + !($3 * $4), 8)\n";
+    FILE *f = fopen("test.fat", "r");
+    if (!f) {
+        printf("Couldn't open test.fat!\n");
+        return 1;
+    }
+    fseek(f, 0L, SEEK_END);
+    size_t flen = ftell(f);
+    fseek(f, 0L, SEEK_SET);
+    char *file_contents = (char*) malloc(flen + 1);
+    size_t read_status;
+    if ((read_status = fread(file_contents, flen, 1, f)) != 1) {
+        printf("Couldn't read test.fat, status = %llu\n", read_status);
+        return 1;
+    }
+    fclose(f);
     Token *tokens;
-    size_t num_tokens = lex(test, &tokens);
+    size_t num_tokens = lex(file_contents, &tokens);
     parse_statement(tokens, num_tokens);
 }
